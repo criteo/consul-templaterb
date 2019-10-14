fa6587349398a07059ba23b5f3d247d9d1ef3dc7
Geoffrey Beausire <g.beausire@criteo.com>
2019-10-14 17:15:34 +0200

    Fix errors on the consul ui page (#49)
    * Fix search on meta for nodes
    
    * Fix filter display

--- samples/consul-ui/js/nodes.js
+++ samples/consul-ui/js/nodes.js
@@ -5,7 +5,9 @@ class ConsulNodesManager extends ConsulUIManager {
             $("#instances-list"),
             $("#instance-filter"),
             $("#instance-counter"),
-            $("#max-display-selector")
+            $("#max-display-selector"),
+            "node_filter",
+            "node_status_filter"
         );
         this.fetchResource();
     }
@@ -16,9 +18,8 @@ class ConsulNodesManager extends ConsulUIManager {
 }
 
 class NodeMainSelector extends MainSelector {
-    constructor(listElement, filterElement, counterElement, maxDisplayed) {
-        super(listElement, filterElement, counterElement, maxDisplayed);
-        this.statusFilter = null;
+    constructor(listElement, filterElement, counterElement, maxDisplayed, URLLabelFilter, URLLabelStatus) {
+        super(listElement, filterElement, counterElement, maxDisplayed, URLLabelFilter, URLLabelStatus);
         this.passingStatusButtonElement = $("#service-status-passing");
         this.warningStatusButtonElement = $("#service-status-warning");
         this.criticalStatusButtonElement = $("#service-status-critical");
@@ -146,8 +147,8 @@ class NodeMainSelector extends MainSelector {
             }
         }
 
-        for (var key in node.meta) {
-            if ((key + ":" + node.meta[key]).match(filter)) {
+        for (var key in node.Node.Meta) {
+            if ((key + ":" + node.Node.Meta[key]).match(filter)) {
                 return true;
             }
         }
